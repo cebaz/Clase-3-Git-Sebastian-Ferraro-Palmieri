8acd7e4c3e3425e28119caad0bf2abe4ffb6f338
cebaz <seba.ferraro123456789@gmail.com>
2025-05-30 14:07:27 -0300

    des-harcode las inverciones

--- main.py
+++ main.py
@@ -13,18 +13,7 @@ clientes = [
 
 acciones = ["APPLE", "TESLA", "NVIDIA"]
 acciones_valores = [10.41, 7.71, 8.50]
-inversiones = [
-    [0, 0, 10],    # Lunatico_pixel invirtió en APPLE (10 unidades)
-    [0, 1, 20],    # Lunatico_pixel invirtió en TESLA (20 unidades)
-    [1, 1, 10],    # Sombra_cristal invirtió en TESLA (10 unidades)
-    [1, 2, 20],    # Sombra_cristal invirtió en NVIDIA (20 unidades)
-    [2, 2, 10],    # Ecoerrante invirtió en NVIDIA (10 unidades)
-    [2, 0, 20],    # Ecoerrante invirtió en APPLE (20 unidades)
-    [3, 0, 15],    # Navefantasma invirtió en APPLE (15 unidades)
-    [4, 1, 25],    # Bytesdelabahia invirtió en TESLA (25 unidades)
-    [5, 2, 30],    # Tintaenelviento invirtió en NVIDIA (30 unidades)
-    [6, 1, 12],    # Relojoxidado invirtió en TESLA (12 unidades)
-]
+inversiones = []
 
 #===============================================MAIN===============================================#
 
